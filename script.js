f9947613d8e46abc78ec5694c86d93f72a63f094
sundarJS-05 <sundar00505@gmail.com>
2024-09-05 16:26:52 +0530

    color changer

---
+++ script.js
@@ -0,0 +1,20 @@
+let buttons = document.querySelectorAll('.button');
+
+const body = document.querySelector('body');
+
+buttons.forEach((btn) => {
+  console.log(btn);
+  btn.addEventListener('click', (event) => {
+    console.log('event - ', event, '\n', 'event.target - ', event.target);
+
+    if (event.target.id === 'grey') {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === 'white') {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === 'blue') {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === 'yellow') {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
